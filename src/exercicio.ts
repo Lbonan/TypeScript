f5c50467fe843584d8a7e89611f33c92ff948eea
Lucas Bonan <lucasbonan15@gmail.com>
2024-08-18 17:40:53 -0300

    adicionando o exercício

---
+++ src/exercicio.ts
@@ -0,0 +1,5 @@
+const multiplicacao = (n1: number, n2: number): number => n1 * n2;
+console.log(multiplicacao(2, 2));
+
+const saudacao = (nome: string): string => `Olá ${nome}`;
+console.log(saudacao("lucas"));
